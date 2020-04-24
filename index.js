7188e633d34e07d35826de115706951b93cf5d21
DatPT <phamtiendatkhanh@gmail.com>
2020-04-24 15:37:47 +0700

    add branch redis

--- index.js
+++ index.js
@@ -1,31 +1,48 @@
 function CacheResponse(redisClient) {
   // const redisClient = redisClient
+  const branch = 'cache-response:'
   function cache(options = {}) {
     return async function (req, res, next) {
-      const duration = options.duration || 12 * 60 * 60;
-      if (!redisClient) return next();
-      const _end = res.end;
-      const _write = res.write;
-      let headers = null;
-      const prefixKey = options.prefix_key;
-
-      let key = options.key ? options.key : req.originalUrl;
+      const duration = options.duration || 12 * 60 * 60
+      if (!redisClient) return next()
+      const _end = res.end
+      const _write = res.write
+      let headers = null
+      const prefixKey = options.prefix_key
+      let key = options.key ? options.key : req.originalUrl
+      let keyTmp = key
       const objectCache = {
         headers,
-        body: null,
-      };
+        body: null
+      }
 
       // only GET method
-      if (req.method.toUpperCase() !== "GET") return next();
+      if (req.method.toUpperCase() !== 'GET') return next()
 
       // check key exists in redis
-      const getcacheResponse = await redisClient.hgetallAsync(key);
-
+      if (prefixKey) {
+        if (typeof prefixKey === 'boolean') {
+          const branchTmp = branch + '*'
+          const keys = await redisClient.keysAsync(branchTmp)
+          if (keys.length > 0) {
+            for (let indexKey = 0; indexKey < keys.length; indexKey++) {
+              const key = keys[indexKey]
+              if (key.includes(req.originalUrl)) {
+                keyTmp = key
+                break
+              }
+            }
+          }
+        } else {
+          keyTmp = `${prefixKey}:${key}`
+        }
+      }
+      const getcacheResponse = await redisClient.hgetallAsync(keyTmp)
       if (getcacheResponse) {
-        res.set(JSON.parse(getcacheResponse.headers)); // Set headers
-        res.setHeader("Last-Modified", new Date().toUTCString());
-        res.send(getcacheResponse.body); // Send body
-        return;
+        res.set(JSON.parse(getcacheResponse.headers)) // Set headers
+        res.setHeader('Last-Modified', new Date().toUTCString())
+        res.send(getcacheResponse.body) // Send body
+        return
       }
 
       /**
@@ -34,26 +51,26 @@ function CacheResponse(redisClient) {
        */
       function hanldeContent(content) {
         if (content) {
-          if (typeof content === "string") {
-            objectCache.body = (objectCache.body || "") + content;
+          if (typeof content === 'string') {
+            objectCache.body = (objectCache.body || '') + content
           } else if (Buffer.isBuffer(content)) {
-            var oldContent = objectCache.body;
+            var oldContent = objectCache.body
 
-            if (typeof oldContent === "string") {
-              oldContent = Buffer.from(oldContent);
+            if (typeof oldContent === 'string') {
+              oldContent = Buffer.from(oldContent)
             }
 
             if (!oldContent) {
-              oldContent = Buffer.alloc(0);
+              oldContent = Buffer.alloc(0)
             }
 
             // Merge chunks
             objectCache.body = Buffer.concat(
               [oldContent, content],
               oldContent.length + content.length
-            );
+            )
           } else {
-            objectCache.body = content;
+            objectCache.body = content
           }
         }
       }
@@ -66,20 +83,21 @@ function CacheResponse(redisClient) {
           try {
             objectCache.headers = res.getHeaders
               ? res.getHeaders()
-              : res._headers;
-            const prefixHeader = objectCache.headers.prefix_key;
-            objectCache.headers = JSON.stringify(objectCache.headers);
+              : res._headers
+            const prefixHeader = objectCache.headers.prefix_key
+            objectCache.headers = JSON.stringify(objectCache.headers)
             if (prefixKey) {
-              if (typeof prefixKey === "boolean") {
-                key = prefixHeader ? `${prefixHeader}:${key}` : key;
+              if (typeof prefixKey === 'boolean') {
+                key = prefixHeader ? `${prefixHeader}:${key}` : key
               } else {
-                key = `${prefixKey}:${key}`;
+                key = `${prefixKey}:${key}`
               }
             }
-            await redisClient.hmsetAsync(key, objectCache);
-            await redisClient.expire(key, duration);
+            key = branch + key
+            await redisClient.hmsetAsync(key, objectCache)
+            await redisClient.expire(key, duration)
           } catch (err) {
-            console.log("[apicache] error in redis.hset()");
+            console.log('[apicache] error in redis.hset()')
           }
         } else {
           // next version use memory cache
@@ -88,39 +106,45 @@ function CacheResponse(redisClient) {
 
       // Overwrite write function to heandle chunk
       res.write = function (content) {
-        hanldeContent(content);
-        _write.apply(res, arguments);
-      };
+        hanldeContent(content)
+        _write.apply(res, arguments)
+      }
 
       // Overwrite end function to cache data
       res.end = function (content, encoding) {
-        hanldeContent(content, encoding);
+        hanldeContent(content, encoding)
         // avoid status 304 http
-        res.setHeader("Last-Modified", new Date().toUTCString());
+        res.setHeader('Last-Modified', new Date().toUTCString())
         // Send data to client before caching
-        _end.apply(res, arguments);
+        _end.apply(res, arguments)
         // only cache when statusCode = 200
         if (res.statusCode === 200) {
-          cacheResponse(req.originalUrl, objectCache, duration);
+          cacheResponse(req.originalUrl, objectCache, duration)
         }
-      };
+      }
       // next to middleware
-      next();
-    };
+      next()
+    }
   }
 
   function clear(key) {
     if (key) {
-      redisClient.keys(key, function (err, keys) {
-        if (err) console.log(err);
-        redisClient.del(keys);
-      });
+      const keyInRedis = `${branch}${key}:*`
+      redisClient.keys(keyInRedis, function (err, keys) {
+        if (err) {
+          console.log(err)
+          return
+        }
+        if (keys.length > 0) {
+          redisClient.del(keys)
+        }
+      })
     }
   }
   return {
     cache,
-    clear,
-  };
+    clear
+  }
 }
 
-module.exports = CacheResponse;
+module.exports = CacheResponse
